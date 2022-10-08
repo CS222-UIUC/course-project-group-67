0eb4b01251e2cae9fe49e971a1f75a901a45c065
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2022-10-07 22:44:53 -0500

    netlfix themeing based on figma

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './netflix.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          welcome to Netflix wrapped
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          connect your account
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
