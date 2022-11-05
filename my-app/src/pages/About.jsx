6f917e070ccd0f053c8d47deafb806ef0571fbaa
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2022-11-04 23:43:29 -0500

    netlix themeing

--- my-app/src/pages/About.jsx
+++ my-app/src/pages/About.jsx
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div>
       <h1>About Page</h1>
-    </div>
+
+      <h3>Welcome to our project!</h3>
+      <h3>We are a group of students at the University of Illinois at Urbana Champaign</h3>
+  </div>
   );
 };
   
