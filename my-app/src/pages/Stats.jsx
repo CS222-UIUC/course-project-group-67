5ec6b8bfc577bdd855c7edc0674cbc2005d72948
ayushenagpal <47947387+ayushenagpal@users.noreply.github.com>
2022-11-11 20:52:47 -0600

    Update stats page to connect to Netflix Account

--- my-app/src/pages/Stats.jsx
+++ my-app/src/pages/Stats.jsx
@@ -1,11 +1,13 @@
 import React from "react";
   
-const About = () => {
+const Stats = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <h1>Stats Page</h1>
+      <h3>Here is where you can find all the statistics of your account. Start by simply clicking the button below". 
+    <button>Connect to Netflix Account</button>
     </div>
   );
 };
   
-export default About;
+export default Stats;
