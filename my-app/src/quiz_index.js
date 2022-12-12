c280c37bf2683042e88b547c569fdcc8b448a91c
Achintya Sanjay <55640537+asanjay123@users.noreply.github.com>
2022-12-12 17:57:33 -0600

    README Update

---
+++ my-app/src/quiz_index.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+//import "./styles.css";
+import Header from "./Components/Header";
+import Quiz from "./Components/Quiz";
+
+function App() {
+  return (
+
+    <Fragment>
+
+      {/* header Components */}
+        <Header />
+
+      {/* Quiz Components */}
+        <Quiz />
+      
+    </Fragment> 
+
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
